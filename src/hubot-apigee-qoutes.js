439ba875229662cf1472153929d11f5c535af0ea
mukundha@apigee.com <mukundha@apigee.com>
2015-08-27 09:08:53 -0700

    first commit

---
+++ src/hubot-apigee-qoutes.js
@@ -0,0 +1,38 @@
+// Description
+//   Search for Apigee Quotes by customers
+//
+// Configuration:
+//   HUBOT_APIGEE_QUOTES_URL
+//
+// Commands:
+//   hubot quote <customer> - 
+//
+//
+// Author:
+//   mukundha@apigee.com <mukundha@apigee.com>
+//
+var request = require('request')
+module.exports = function(robot) {
+	robot.respond(/qoute (.*)/i, function(msg){           	
+        var account = msg.match[1]
+        getQuote(account,msg)
+    })
+}
+
+function getQuote(account,msg){
+	var url = process.env.HUBOT_APIGEE_QUOTES_URL
+	var ql = 'select * where customer contains \'' + account + '\''
+	request({
+        url:url + '?ql=' + ql 
+    },function(error,response,body){
+		var b = JSON.parse(body)
+		b.entities.forEach(function(e){
+			var by = e.by
+			if(e.title){
+				by+= ',' + e.title
+			}
+		  	msg.send('>' + by)
+		  	msg.send('```' + e.quote + '```')
+		})
+	})
+}
